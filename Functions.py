fbd956ab20a19200fe460f521a02e1f26681081c
Chayan Kathuria <chayankathuria@gmail.com>
2020-02-05 10:39:49 +0530

    Added load & norm

---
+++ Functions.py
@@ -0,0 +1,22 @@
+# load train and test dataset
+def load_dataset():
+    # load dataset
+    (trainX, trainY), (testX, testY) = mnist.load_data()
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
+    
+# scale pixels
+def norm_pixels(train, test):
+    # convert from integers to floats
+    train_norm = trainX.astype('float32')
+    test_norm = testX.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
